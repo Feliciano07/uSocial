275f10f84dccbaf874a14daa09a56390d39d081d
Feliciano07 <fernandoxhajon122@gmail.com>
2020-10-11 11:59:12 -0600

    Configuracion inicial del servidor

---
+++ server/build/app.js
@@ -0,0 +1,56 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.App = void 0;
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
+class App {
+    constructor(port) {
+        this.port = port;
+        this.app = express_1.default();
+        this.settings();
+        this.middlewares();
+    }
+    settings() {
+        this.app.set('port', this.port || process.env.PORT || 3000);
+        this.app.use(cors_1.default());
+        //cors
+        this.app.use(function (req, res, next) {
+            // Website you wish to allow to connect
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            // Request methods you wish to allow
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+            // Request headers you wish to allow
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+            // Set to true if you need the website to include cookies in the requests sent
+            // to the API (e.g. in case you use sessions)
+            res.setHeader('Access-Control-Allow-Credentials', 1);
+            // Pass to next layer of middleware
+            next();
+        });
+    }
+    middlewares() {
+        this.app.use(morgan_1.default('dev'));
+        this.app.use(express_1.default.json());
+        this.app.use(express_1.default.urlencoded({ extended: false }));
+    }
+    listen() {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield this.app.listen(this.app.get('port'));
+            console.log('Server on port', this.app.get('port'));
+        });
+    }
+}
+exports.App = App;
---
+++ server/src/app.ts
@@ -0,0 +1,55 @@
+import express, {Application, urlencoded} from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+export class App {
+
+
+    private app: Application;
+
+    constructor(private port?: number | string){
+        this.app = express();
+        this.settings();
+        this.middlewares();
+    }
+
+    settings(): void{
+        this.app.set('port', this.port || process.env.PORT || 3000 );
+
+        this.app.use(cors());
+
+                //cors
+                this.app.use(function (req, res, next) {
+
+                    // Website you wish to allow to connect
+                    res.setHeader('Access-Control-Allow-Origin', '*');
+        
+                    // Request methods you wish to allow
+                    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        
+                    // Request headers you wish to allow
+                    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        
+                    // Set to true if you need the website to include cookies in the requests sent
+                    // to the API (e.g. in case you use sessions)
+                    res.setHeader('Access-Control-Allow-Credentials', 1);
+        
+                    // Pass to next layer of middleware
+                    next();
+                });
+    }
+
+    middlewares(): void{
+        this.app.use(morgan('dev'));
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: false}));
+    }
+
+    async listen(){
+        await this.app.listen(
+            this.app.get('port')
+        );
+        console.log('Server on port', this.app.get('port'));
+    }
+
+}
