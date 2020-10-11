1c8a97649dad242ce98c1b352735075d87862dcf
Feliciano07 <fernandoxhajon122@gmail.com>
2020-10-11 12:07:41 -0600

    Componentes iniciales en angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,20 @@ import { AppComponent } from './app.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    PerfilComponent,
+    InicioComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
