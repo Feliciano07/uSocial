56349254b171830c45b80eeca036fd87e50034fc
Gerardo cHAY <2995058550101@ingenieria.usac.edu.gt>
2020-10-12 17:15:03 -0600

    Diseño de chat

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
+  channelList = []
+  newMessage = ''
+  username = '';
+  title = 'angular-chat';
+  messages: any[] = [];
+
+  currentUser:any = {
+    id: 1,
+    name: 'Chay'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    this.channelList.push({name:'Gerardo',fecha:'12/10/2020 17:08'})
+    this.messages.push({
+      id: 1,
+      name: 'Chay',
+      text: 'Hola BB',
+      fecha:'12/10/2020 17:08'
+    })
+    this.messages.push({
+      id: 2,
+      name: 'David',
+      text: 'Hi there',
+      fecha:'12/10/2020 17:12'
+    })
   }
+
 
 }
