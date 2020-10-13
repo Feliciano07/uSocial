639a9f00359770c533cd4470aa89e09efafd5a37
Feliciano07 <fernandoxhajon122@gmail.com>
2020-10-13 13:50:17 -0600

    Se agrego el modal para la creacion de nuevas publicaciones

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+interface HtmlInputEvent extends Event {
+  target: HTMLInputElement & EventTarget;
+}
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
@@ -13,10 +17,34 @@ export class InicioComponent implements OnInit {
     {nombre: 'zapatosdddddddddddddddddddddddddddddddddd'}
   ];
 
+  
+  photoSelected: string | ArrayBuffer; // para mostrar la previsualir
+  private selectedFile: ImageSnippet; // manejar la imagen a subir
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // Seleccionar fotos
+  onPhotoSelected(event: HtmlInputEvent): void {
+    if (event.target.files && event.target.files[0]) {
+      const file:File = <File>event.target.files[0];
+      // image preview
+      const reader = new FileReader();
+      reader.onload = e => this.photoSelected = reader.result;
+      reader.readAsDataURL(file);
+
+      reader.addEventListener('load', (even: any) => {
+        this.selectedFile = new ImageSnippet(even.target.result, file);
+      })
+
+
+    }
+  }
+
+}
+
+class ImageSnippet {
+  constructor(public src: string, public file: File) {}
 }
