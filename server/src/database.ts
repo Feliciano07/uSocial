64f745f20b716558e76e3cf07622ecf34bde644a
Gerardo cHAY <2995058550101@ingenieria.usac.edu.gt>
2020-10-14 12:08:09 -0600

    acceso a database sql

---
+++ server/src/database.ts
@@ -0,0 +1,26 @@
+import mysql from 'mysql';
+import keys from './keys';
+const { promisify } = require('util'); //final
+const pool = mysql.createPool(keys.database);//modulo de conexion de la base de datos
+//pool servira para comenzar la conexion
+pool.getConnection((err, conn) =>{
+  if (err) {
+      if (err.code == 'PROTOCOL_CONNECTION_LOST'){
+          console.error('LA CONEXIONES DE LA BASE DE DATOS FUE CERRADA');
+      }
+     if(err.code == 'ER_CON_COUNT_ERROR') {
+          console.error('LA BASE DE DATOS TIENE MUCHAS CONEXIONES');
+     }
+     if (err.code == 'ECONNREFUSED') {
+         console.error('LA CONEXIONES DE LA BASE DE DATOS FUE RECHAZADA');
+     }
+  }
+  if (conn) conn.release();
+  console.log('BD is Connected');
+  return;
+});
+
+// esto es una promesa
+pool.query = promisify(pool.query); // final
+
+export default pool;
---
+++ server/src/keys.ts
@@ -0,0 +1,9 @@
+export default{//encargado de la configuracion de db
+
+  database:{
+      host: 'localhost',
+      user: process.env.USER_DB,
+      password: process.env.PWD_DB,
+      database: 'red'
+  }
+}
